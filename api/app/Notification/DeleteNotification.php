750d05f325ff19f364c7e38107e62a3bee71c1a4
waqasahmedNU <k122193@nu.edu.pk>
2020-07-09 02:05:17 +0200

    Notification Code Changes

--- api/app/Notification/DeleteNotification.php
+++ api/app/Notification/DeleteNotification.php
@@ -3,44 +3,11 @@
 namespace App\Notification;
 
 
-use Illuminate\Broadcasting\Channel;
-use Illuminate\Bus\Queueable;
-use Illuminate\Notifications\Notification;
-use Illuminate\Support\Facades\Redis;
-
-class DeleteNotification extends Notification
+class DeleteNotification extends NotificationController
 {
-    use Queueable;
     protected $appNotification;
 
     public function __construct($appNotification){
         $this->appNotification = $appNotification;
-    }
-
-    public function via($notifiable){
-        return ['database', 'broadcast'];
-    }
-
-    public function toDatabase($notifiable)
-    {
-        return [
-            'id' => $this->appNotification->id,
-            'name' => $this->appNotification->name,
-        ];
-    }
-
-    public function toBroadcast($notifiable)
-    {
-        $count = Redis::get('count');
-        $count = $count + 1;
-        Redis::set('count', $count);
-        return [
-            'count' => $count
-        ];
-    }
-
-    public function broadcastOn()
-    {
-        return new Channel('notification_channel');
     }
 }
---
+++ api/app/Notification/NotificationController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Notification;
+
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Redis;
+
+class NotificationController extends Notification
+{
+    use Queueable;
+    protected $appNotification;
+
+    public function via($notifiable){
+        return ['database', 'broadcast'];
+    }
+
+    public function toDatabase($notifiable)
+    {
+        return [
+            'id' => $this->appNotification->id,
+            'name' => $this->appNotification->name,
+        ];
+    }
+
+    public function toBroadcast($notifiable)
+    {
+        $count = Redis::get('count');
+        $count = $count + 1;
+        Redis::set('count', $count);
+        return [
+            'count' => $count
+        ];
+    }
+
+    public function broadcastOn()
+    {
+        return new Channel('notification_channel');
+    }
+}
